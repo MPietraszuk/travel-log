6af8335175b2a48bc44e4fe25b995518be917814
mpietraszuk <GitHub@MarkZDesign.com>
2021-01-12 14:54:33 -0800

    EntryFormClient07

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { add } from 'lodash';
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
@@ -8,6 +9,7 @@ import { listLogEntries } from './api';
 const App = () => {
   const [LogEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -23,12 +25,21 @@ const App = () => {
     })();
   }, []);
 
+  const showAddMarkerPopup = (event) => {
+    const [ longitude, latitude ] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    });
+  };
+
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/mpietraszuk/ckjtz9y9y0tp41alimi3im1r0"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
+      onDblClick={showAddMarkerPopup}
     >
       {
         LogEntries.map(entry => (
@@ -81,6 +92,46 @@ const App = () => {
             }
             </>
         ))
+      }
+      {
+        addEntryLocation ? (
+          <>
+          <Marker 
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+          >
+              <div>
+                <svg
+                  className="marker red"
+                  style={{
+                    height: `${6 * viewport.zoom}px`,
+                    width: `${6 * viewport.zoom}px`,
+                  }}
+                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                  <g>
+                    <g>
+                      <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                        c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                        c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </Marker>
+          <Popup
+              latitude={addEntryLocation.latitude}
+              longitude={addEntryLocation.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setAddEntryLocation(null)}
+              anchor="top" >
+              <div className="popup">
+                <h3>Log Your New Place!</h3>
+              </div>
+          </Popup>
+          </>
+        ) : null
       }
     </ReactMapGL>
   );
