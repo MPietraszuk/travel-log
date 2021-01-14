00e6a706cb0aa046294a43a29f676fa3ec089b5f
mpietraszuk <GitHub@MarkZDesign.com>
2021-01-14 06:57:51 -0800

    EntryFormClient08

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import { add } from 'lodash';
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 // import logo from './logo.svg';
 
 import { listLogEntries } from './api';
+import LogEntryForm from './LogEntryForm';
 
 const App = () => {
   const [LogEntries, setLogEntries] = useState([]);
@@ -18,11 +18,13 @@ const App = () => {
     zoom: 3
   });
 
+  const getEntries = async () => {
+    const LogEntries = await listLogEntries();
+    setLogEntries(LogEntries);
+  }
+
   useEffect(() => {
-    (async () => {
-      const LogEntries = await listLogEntries();
-      setLogEntries(LogEntries);
-    })();
+    getEntries();
   }, []);
 
   const showAddMarkerPopup = (event) => {
@@ -43,9 +45,8 @@ const App = () => {
     >
       {
         LogEntries.map(entry => (
-          <>
+          <React.Fragment key={entry._id}>
           <Marker 
-            key={entry._id}
             latitude={entry.latitude}
             longitude={entry.longitude}
           >
@@ -62,13 +63,11 @@ const App = () => {
                     width: `${6 * viewport.zoom}px`,
                   }}
                   version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
-                  <g>
                     <g>
                       <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
                         c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
                         c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
                     </g>
-                  </g>
                 </svg>
               </div>
             </Marker>
@@ -86,11 +85,12 @@ const App = () => {
                   <h3>{entry.title}</h3>
                   <p>{entry.comments}</p>
                   <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                  {entry.image && <img src={entry.image} alt={entry.title} />}
                 </div>
             </Popup>
               ) : null
             }
-            </>
+            </React.Fragment>
         ))
       }
       {
@@ -127,7 +127,10 @@ const App = () => {
               onClose={() => setAddEntryLocation(null)}
               anchor="top" >
               <div className="popup">
-                <h3>Log Your New Place!</h3>
+                <LogEntryForm onClose={() => {
+                  setAddEntryLocation(null);
+                  getEntries();
+                }} location={addEntryLocation} />
               </div>
           </Popup>
           </>
